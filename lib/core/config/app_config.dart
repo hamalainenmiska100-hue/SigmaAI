2efafe5df0c4c32351c674ec9bbb3c38a658035b
hamalainenmiska100-hue <hamalainen.miska100@gmail.com>
2026-04-24 08:56:58 +0300

    Update proxyChatUrl to remove '/chat' suffix

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,5 +1,5 @@
 class AppConfig {
-  static const String proxyChatUrl = 'https://chat.vymedia.xyz/chat';
+  static const String proxyChatUrl = 'https://chat.vymedia.xyz';
 
   static const int maxMessageLength = 4000;
   static const int maxCustomInstructionsLength = 2000;
